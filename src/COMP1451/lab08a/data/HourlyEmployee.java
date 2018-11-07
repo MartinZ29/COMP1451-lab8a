625c021368ca0b70823ee7a28e10f49a5781ab2a
Martin Zhang <mtnz6748@gmail.com>
2018-11-06 21:12:52 -0800

    constructor getters and setters

--- src/COMP1451/lab08a/data/HourlyEmployee.java
+++ src/COMP1451/lab08a/data/HourlyEmployee.java
@@ -2,4 +2,30 @@ package COMP1451.lab08a.data;
 
 public class HourlyEmployee {
 
+	private int hoursWorked;
+	public final int HOURLY_WAGE = 50;
+	
+	public HourlyEmployee() {
+		super(); 
+	}
+	
+	public HourlyEmployee(int hoursWorked) {
+		super();
+		this.hoursWorked = hoursWorked;
+	}
+	
+	public int getHoursWorked() {
+		return hoursWorked;
+	}
+	
+	public void setHoursWorked(int hoursWorked) {
+		this.hoursWorked = hoursWorked;
+	}
+	
+	@Override
+	public String toString() {
+		return "HourlyEmployee [hoursWorked=" + hoursWorked + ", HOURLY_WAGE=" + HOURLY_WAGE + "]";
+	}
+	
+	
 }
