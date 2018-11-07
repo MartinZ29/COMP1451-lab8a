bc2d2e3c622ef777078d2c377f90eb53bb1c4521
Martin Zhang <mtnz6748@gmail.com>
2018-11-06 21:09:11 -0800

    Add getters setters and toString()

--- src/COMP1451/lab08a/data/Employee.java
+++ src/COMP1451/lab08a/data/Employee.java
@@ -5,5 +5,36 @@ public abstract class Employee {
 	private String firstName;
 	private String lastName;
 	
+	public Employee() {
+		super();
+	}
+	
+	public Employee(String firstName, String lastName) {
+		super();
+		setFirstName(firstName);
+		setLastName(lastName);
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	@Override
+	public String toString() {
+		return "Employee [firstName=" + firstName + ", lastName=" + lastName + "]";
+	}
+	
 	
 }
