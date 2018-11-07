041be8874e8ff6dd306bbcd25aa069c3dc9bf0a4
Martin Zhang <mtnz6748@gmail.com>
2018-11-06 21:20:12 -0800

    Add objects to test

--- src/COMP1451/lab08a/driver/EmployeeDriver.java
+++ src/COMP1451/lab08a/driver/EmployeeDriver.java
@@ -6,6 +6,11 @@ public class EmployeeDriver {
 
 	public static void main(String[] args) {
 		
+		Employee hourlyEmployee = new HourlyEmployee("abc","edf", 160);
+		Employee commissionEmployee = new CommissionEmployee("uvw", "xyz", 0.5, 20000);
+		
+		System.out.println(hourlyEmployee.calculateMonthlyEarnings());
+		System.out.println(commissionEmployee.calculateMonthlyEarnings());
 
 	}
 
