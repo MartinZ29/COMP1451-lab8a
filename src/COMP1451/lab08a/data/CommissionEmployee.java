b1b6dc9acc42c14998cfa768d9a0e82363df09c8
Martin Zhang <mtnz6748@gmail.com>
2018-11-06 21:16:00 -0800

    constructor, getters setters

--- src/COMP1451/lab08a/data/CommissionEmployee.java
+++ src/COMP1451/lab08a/data/CommissionEmployee.java
@@ -2,4 +2,42 @@ package COMP1451.lab08a.data;
 
 public class CommissionEmployee {
 
+	private double commissionRate;
+	private double monthlySalesTotal;
+	
+	
+	public CommissionEmployee() {
+		super();
+	}
+
+	public CommissionEmployee(double commissionRate, double monthlySalesTotal) {
+		super();
+		this.commissionRate = commissionRate;
+		this.monthlySalesTotal = monthlySalesTotal;
+	}
+
+	public double getCommissionRate() {
+		return commissionRate;
+	}
+
+	public void setCommissionRate(double commissionRate) {
+		this.commissionRate = commissionRate;
+	}
+
+	public double getMonthlySalesTotal() {
+		return monthlySalesTotal;
+	}
+
+	public void setMonthlySalesTotal(double monthlySalesTotal) {
+		this.monthlySalesTotal = monthlySalesTotal;
+	}
+
+	@Override
+	public String toString() {
+		return "CommissionEmployee [commissionRate=" + commissionRate + ", monthlySalesTotal=" + monthlySalesTotal
+				+ "]";
+	};
+	
+	
+	
 }
